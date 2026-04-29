33df3d55b33d9aa331d96c0512d0614e084816b1
Winda <247006111054@student.unsil.ac.id>
2026-04-29 15:57:58 +0700

    edit sejarah distribusi

--- sejarah-distribusi/script.js
+++ sejarah-distribusi/script.js
@@ -53,28 +53,23 @@ function renderTable() {
     var realIdx = start + idx;
     var tr = document.createElement("tr");
     tr.innerHTML =
-      "<td>" + row.tglDistribusi + "</td>" +
-      "<td>" + row.pelanggan + "</td>" +
-      "<td>" + row.tglProduksi + "</td>" +
-      "<td>" + row.jumlah + "</td>" +
-      "<td>" +
-        "<button class='edit-btn' onclick='openModal(" + realIdx + ")' title='Edit'>" +
-          "<svg width='15' height='15' fill='none' stroke='currentColor' stroke-width='1.8' viewBox='0 0 24 24'>" +
-            "<path d='M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7'/>" +
-            "<path d='M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z'/>" +
-          "</svg>" +
-        "</button>" +
-      "</td>" +
-      "<td>" +
-        "<select class='status-select' onchange='updateStatus(" + realIdx + ", this.value)'>" +
-          optionsHtml(row.status) +
-        "</select>" +
-      "</td>" +
-      "<td>" +
-        "<svg width='14' height='14' fill='none' stroke='#bbb' stroke-width='2' viewBox='0 0 24 24'>" +
-          "<path d='M6 9l6 6 6-6'/>" +
-        "</svg>" +
-      "</td>";
+  "<td>" + row.tglDistribusi + "</td>" +
+  "<td>" + row.pelanggan + "</td>" +
+  "<td>" + row.tglProduksi + "</td>" +
+  "<td>" + row.jumlah + "</td>" +
+  "<td>" +
+    "<button class='edit-btn' onclick='openModal(" + realIdx + ")' title='Edit'>" +
+      "<svg width='15' height='15' fill='none' stroke='currentColor' stroke-width='1.8' viewBox='0 0 24 24'>" +
+        "<path d='M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7'/>" +
+        "<path d='M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z'/>" +
+      "</svg>" +
+    "</button>" +
+  "</td>" +
+  "<td>" +
+    "<select class='status-select' onchange='updateStatus(" + realIdx + ", this.value)'>" +
+      optionsHtml(row.status) +
+    "</select>" +
+  "</td>";
     tbody.appendChild(tr);
   });
 
